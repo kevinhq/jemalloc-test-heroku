2e4629ab010ba2e9b06228aab893a5ecb763c93e
kevin <1758073+kevinhq@users.noreply.github.com>
2021-04-25 18:46:29 +0700

    fix error

--- config/application.rb
+++ config/application.rb
@@ -7,19 +7,19 @@ require "active_model/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
-# require "action_mailer/railtie"
+require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
 require "action_view/railtie"
 # require "action_cable/engine"
-require "sprockets/railtie"
-require "rails/test_unit/railtie"
+# require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module JemallocTest
+module Newthl
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
